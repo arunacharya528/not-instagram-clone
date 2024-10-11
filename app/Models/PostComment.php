1fe57370dffab9c56fc6836ed411fc2227e6075c
Arun Acharya <arunacharya255@gmail.com>
2024-10-11 19:04:44 +0545

    feat: implement `Post Comment`

---
+++ app/Models/PostComment.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class PostComment extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'post_id',
+        'platform_user_id',
+        'content',
+    ];
+
+    public function post()
+    {
+        return $this->belongsTo(Post::class);
+    }
+
+    public function platformUser()
+    {
+        return $this->belongsTo(PlatformUser::class);
+    }
+}
